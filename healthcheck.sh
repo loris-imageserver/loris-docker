8287dc7bdaaec05864dab21f46eacbc1e7f6a2c6
Luke <lsh-0@users.noreply.github.com>
2020-03-17 15:15:24 +1030

    disables healthcheck. container is now talking uwsgi, not http

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -e
-[ "$(curl --silent --output /dev/null --write '%{http_code}' http://localhost:5004/)" == 200 ]
+# 2020-03: disabled, uwsgi is now talking uwsgi not http. how to test uWSGI is available?
+#[ "$(curl --silent --output /dev/null --write '%{http_code}' http://localhost:5004/)" == 200 ]
