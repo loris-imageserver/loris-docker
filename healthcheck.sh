5df6fc8153c97b7523bcc2b7f9daadd52b0de774
Luke <lsh-0@users.noreply.github.com>
2020-03-17 15:30:29 +1030

    replaced gnu-netcat callout with an openbsd-netcat one fml

--- healthcheck.sh
+++ healthcheck.sh
@@ -5,4 +5,8 @@ set -e
 
 # tests that a socket is open and exits immediately with 1 or 0
 # not as good as the http test above
-nc --zero --verbose localhost 5004
+#nc --zero --verbose localhost 5004 # gnu-netcat
+
+# -w timeout (seconds)
+# -v verbose
+nc localhost 5004 -v -w 1
