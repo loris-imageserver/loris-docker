02965e5f1844cd7aa18d2f5b73d8d689ace27c66
Luke <lsh-0@users.noreply.github.com>
2020-03-17 15:21:56 +1030

    replacement healthcheck

--- healthcheck.sh
+++ healthcheck.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
-# 2020-03: disabled, uwsgi is now talking uwsgi not http. how to test uWSGI is available?
+# 2020-03: disabled, uwsgi is now talking uwsgi not http.
 #[ "$(curl --silent --output /dev/null --write '%{http_code}' http://localhost:5004/)" == 200 ]
+
+# tests that a socket is open and exits immediately with 1 or 0
+# not as good as the http test above
+nc --zero --verbose localhost 5004
